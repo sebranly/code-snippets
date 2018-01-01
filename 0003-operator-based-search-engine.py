db2289497529d65106a5901b045b966c9b421fb9
Sebastien Branly <sebranly.pinf@gmail.com>
2018-01-01 19:15:44 +0100

    feat: add a beginning of implementation for the operator-based search engine

---
+++ 0003-operator-based-search-engine.py
@@ -0,0 +1,120 @@
+all_elements = [
+    ["rap", "us", "english", "eminem", "movie_related"], # Eminem - Lose Yourself
+    ["rap", "us", "english", "tupac", "diss_song"], # Tupac - Hit 'Em Up
+    ["rap", "france", "french", "nekfeu"], # Nekfeu - On Verra
+    ["rap", "us", "english", "eminem", "ed_sheeran", "feat"], # Eminem - River ft. Ed Sheeran
+    ["france", "french", "edith_piaf", "old"], # Edith Piaf - Non, je ne regrette rien
+    ["england", "english", "ed_sheeran"] # Ed Sheeran - Shape of You
+]
+
+def elements_are_identical(elements_ids_1, elements_ids_2):
+    if len(elements_ids_1) != len(elements_ids_2):
+        return False
+    i = 0
+    while i < len(elements_ids_1):
+        if (elements_ids_1[i] != elements_ids_2[i]):
+            return False
+        i += 1
+    return True
+
+def element_contains_label(element, label):
+    for element_label in element:
+        if element_label == label:
+            return True
+    return False
+
+def get_all_elements_ids():
+    matching_elements_ids = []
+    i = 0
+    while i < len(all_elements):
+        matching_elements_ids.append(i)
+        i += 1
+    return matching_elements_ids
+
+def get_all_elements_ids_except_label(exception_label):
+    matching_elements_ids = []
+    i = 0
+    while i < len(all_elements):
+        if not element_contains_label(all_elements[i], exception_label):
+            matching_elements_ids.append(i)
+        i += 1
+    return matching_elements_ids
+
+def get_elements_ids_based_on_label(label):
+    matching_elements_ids = []
+    i = 0
+    while i < len(all_elements):
+        if element_contains_label(all_elements[i], label):
+            matching_elements_ids.append(i)
+        i += 1
+    return matching_elements_ids
+
+def get_union_ids(elements_ids_1, elements_ids_2):
+    return list(set(elements_ids_1 + elements_ids_2))
+
+def get_intersection_ids(elements_ids_1, elements_ids_2):
+    return list(set(elements_ids_1).intersection(elements_ids_2))
+
+def get_symmetric_difference_ids(elements_ids_1, elements_ids_2):
+    if len(elements_ids_1) < len(elements_ids_2):
+        return list(set(elements_ids_1).symmetric_difference(elements_ids_2))
+    return list(set(elements_ids_2).symmetric_difference(elements_ids_1))
+
+def get_elements(search_query):
+    if (search_query == "ALL"):
+        return get_all_elements_ids()
+    # Unary operator
+    if ("NOT" in search_query):
+        decomposed_search_query = search_query.split(' ')
+        return get_all_elements_ids_except_label(decomposed_search_query[1])
+    # Binary operators
+    if ("OR" in search_query or "AND" in search_query or "XOR" in search_query):
+        decomposed_search_query = search_query.split(' ')
+        matching_elements_ids_1 = get_elements_ids_based_on_label(decomposed_search_query[0])
+        matching_elements_ids_2 = get_elements_ids_based_on_label(decomposed_search_query[2])
+        if ("XOR" in search_query):
+            return get_symmetric_difference_ids(matching_elements_ids_1, matching_elements_ids_2)
+        if ("OR" in search_query):
+            return get_union_ids(matching_elements_ids_1, matching_elements_ids_2)
+        if ("AND" in search_query):
+            return get_intersection_ids(matching_elements_ids_1, matching_elements_ids_2)
+    return get_elements_ids_based_on_label(search_query)
+    return []
+
+# Tests
+
+results = get_elements("ALL")
+print(elements_are_identical(results, [0, 1, 2, 3, 4, 5]))
+
+results = get_elements("france")
+print(elements_are_identical(results, [2, 4]))
+
+results = get_elements("germany")
+print(elements_are_identical(results, []))
+
+results = get_elements("NOT us")
+print(elements_are_identical(results, [2, 4, 5]))
+
+results = get_elements("NOT germany")
+print(elements_are_identical(results, [0, 1, 2, 3, 4, 5]))
+
+results = get_elements("eminem OR tupac")
+print(elements_are_identical(results, [0, 1, 3]))
+
+results = get_elements("macklemore OR snoop_dogg")
+print(elements_are_identical(results, []))
+
+results = get_elements("eminem AND ed_sheeran")
+print(elements_are_identical(results, [3]))
+
+results = get_elements("eminem AND tupac")
+print(elements_are_identical(results, []))
+
+results = get_elements("eminem XOR ed_sheeran")
+print(elements_are_identical(results, [0, 5]))
+
+results = get_elements("rap XOR us")
+print(elements_are_identical(results, [2]))
+
+results = get_elements("macklemore XOR snoop_dogg")
+print(elements_are_identical(results, []))
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # code-snippets
 This repository gathers diverse tiny challenges I encountered on YouTube, Facebook, Twitter (or other social media) that I found interesting enough to share
 
-## 0001 - Micmaths 9-digit Enigma
+## 0001 - Micmaths 9-digit enigma
 
 Mickaël Launay alias Micmaths is a French 250K-subscriber YouTuber. As his channel is now 10 years old, he decided to organize a YouTube Live in November 2017. During this live, he challenged us with an enigma: find the only solution to `42 x _ _ _ = _ _ _ _` so that each `_` is a digit and digits from 1 to 9 should appear only once in the equation. Here's the link: https://www.youtube.com/watch?v=vYa930zDtz4&t=13m
 - Finding the solution to this enigma was actually pretty easy because as 2 and 4 are already taken, we naively try out `42 * 135` then `42 * 136` then `42 * 137` and finally we find after 4 attempts that `42 * 138` is the solution, without actually thinking about the problem in detail. I still decided to implement a naive implementation of the solution to this enigma by also looking for other starting numbers than just `42`
@@ -36,3 +36,37 @@ On this website I encountered the following challenge under the `Uber Bot` categ
 0 +------+------+------+ ...
   0 0.4  1      2      3
 ```
+
+## 0003 - Operator-based search-engine (Work In Progress)
+
+### Context
+
+I love listening to music, especially on YouTube because I can find good fan-made songs. As my favorite kind of music is rap, everytime I find a good rap song I add it to a playlist. This playlist has now over 250 songs. Sometimes I'm in the mood for French songs but not English ones, sometimes it's the opposite. Sometimes I'm in the mood for certain specific artists but I'd like to exclude diss songs because the context might not be appropriate for example.
+
+So I came up with an idea: I'd love to have a system that enables me to write a specific query that gets applied to my 250+ song playlist (the input) in order to give the matching results as an output, based on tags. I could for example execute the following query: `eminem OR tupac AND NOT diss_song`.
+- Note: such a system could be applied to a lot of domains beyond music (movies, TV shows, pictures, job postings, etc.) as long as the individual elements get correctly tagged beforehand
+
+### Implementation
+
+#### Existing state
+
+- The proof of concept of such a search engine that I coded in Python handles the following operators:
+  - Unary operators
+    - `NOT` (e.g. `NOT diss_song` in order to exclude diss songs)
+  - Binary operators
+    - `AND` (e.g. `eminem AND tupac` in order to get songs featuring both Eminem and Tupac)
+    - `OR` (e.g. `eminem OR tupac` in order to get both songs by Eminem and songs by Tupac)
+	- `XOR` (e.g. `eminem XOR tupac` in order to get both songs by Eminem and songs by Tupac, by excluding songs featuring both of them though)
+  - Others
+	- `ALL` (e.g. `ALL` in order to get all the songs from the playlist)
+- It is also possible to only type a label (e.g. `eminem` in order to get songs by Eminem)
+
+In order to check this search engine is correctly implemented, I added tests at the end of it with expected results. Everything should be `True`.
+	
+#### Future state (not implemented yet)
+
+- The search engine is really limited for now.
+  - Some operators are still missing: `NAND`, `NOR`, `XNOR`
+  - It should be possible to execute more complex queries including more than just two labels (e.g. `eminem AND tupac AND nekfeu`)
+  - It should also be possible to use parentheses (`(` and `)`) in order to give a certain priority.
+- In the long-term, a nested query like `NOT (eminem AND (tupac OR snoop_dogg))` should be correct
